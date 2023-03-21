12c915f96ad2125e3c16ce7b907db481c597b6b6
Yannis <kessas.belhadj@gmail.com>
2023-03-21 14:26:52 +0100

    Initial Commit

---
+++ FindMyCoach/FindMyCoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindMyCoachApp.swift
+//  FindMyCoach
+//
+//  Created by Yannis Kessas on 21/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct FindMyCoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
